dc43e5000c186e0f025c2244e0cc0a27efe98507
maiolPons <100287697+maiolPons@users.noreply.github.com>
2022-11-13 06:13:01 -0800

    Delete ProjecteM12 directory

--- ProjecteM12/ProjecteM12/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.projectem12 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    opens com.mycompany.projectem12 to javafx.fxml;
-    opens com.mycompany.controlAccess;
-    exports com.mycompany.projectem12;
-}
